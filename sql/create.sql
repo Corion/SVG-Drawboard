5d6f60b7950025732cf8643c2c1f4da9b77ec32c
Max Maischein <corion@corion.net>
2020-10-27 18:57:09 +0100

    Tentatively create an index for the latest message
    How often do we append to the table vs. how often do we want the latest
    message for each element? For now, the index doesn't cost us anything,
    so we have it ...

--- sql/create.sql
+++ sql/create.sql
@@ -5,3 +5,7 @@ create table drawboard_items (
     properties varchar(1024), -- a JSON string, eww
     timestamp  datetime DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Create an index so we can easily pick the latest action for each item
+create index idx_drawboard_items_latest
+    on drawboard_items(drawboard,timestamp,item);
